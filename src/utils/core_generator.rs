d49faeb43f9d6aaea76924c8a5a3c4f0f08d63be
stephenasuncionDEV <stephenasuncion02@gmail.com>
2022-08-22 22:25:01 -0700

    🌻UPDATE: finished version 1 of generator

--- src/utils/core_generator.rs
+++ src/utils/core_generator.rs
@@ -1,8 +1,9 @@
 use crate::{structs, info, debug};
 use wasm_bindgen::prelude::*;
-use photon_rs::{multiple, base64_to_image};
 use rand::prelude::*;
 use rand::distributions::WeightedIndex;
+use photon_rs::{PhotonImage, base64_to_image, multiple, to_image_data};
+use web_sys::{ImageData, CanvasRenderingContext2d};
 
 pub fn randomized_image(layer: structs::Layer) -> structs::Image {
     let cur_images: Vec<structs::Image> = layer.images.clone();
@@ -13,13 +14,13 @@ pub fn randomized_image(layer: structs::Layer) -> structs::Image {
     .collect();
 
     let dist: WeightedIndex<i32> = WeightedIndex::new(&weights).unwrap();
-    let mut rng = thread_rng();
+    let mut rng: ThreadRng = thread_rng();
 
     cur_images[dist.sample(&mut rng)].clone()
 }
 
 #[wasm_bindgen]
-pub fn generate(constructed_layers: &JsValue) {
+pub fn generate(ctx: &CanvasRenderingContext2d, constructed_layers: &JsValue) {
     info!("[gen-rs] generator started");
 
     let constructed_layers_input: Vec<structs::Layer> = constructed_layers.into_serde().unwrap();
@@ -29,9 +30,21 @@ pub fn generate(constructed_layers: &JsValue) {
     .map(|layer: structs::Layer| randomized_image(layer))
     .collect();
 
-    debug!("{:?}", images_chosen);
+    let images_chosen_photon: Vec<PhotonImage> = images_chosen
+    .into_iter()
+    .map(|image| base64_to_image(&image.src[22..]))
+    .collect();
 
-    // debug!("{:?}", constructed_layers_input);
+    let mut base_image: PhotonImage = images_chosen_photon[0].to_owned();
+
+    for n in 1..images_chosen_photon.len() {
+        multiple::watermark(&mut base_image, &images_chosen_photon[n], 30, 40);
+        debug!("{:?}", base_image);
+    }
+
+    let generated_image: ImageData = to_image_data(base_image);
+
+    ctx.put_image_data(&generated_image, 0.0, 0.0);
 
     info!("[gen-rs] generator finished");
 }
